ee747b1dbe96e886dcfddd5c535c6fda49d90472
Biola <52200708+decorouz@users.noreply.github.com>
2022-05-17 19:44:09 +0100

    create tag and tagitem classes

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,16 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TagItem(models.Model):
+    # Type -> Product, Video, Project, Blog Post, Course
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    target_ct = models.ForeignKey(ContentType, on_delete=models.CASCADE)  # content type
+    target_id = models.PositiveIntegerField()  # object id
+    target = GenericForeignKey("target_ct", "target_id")  # content object
