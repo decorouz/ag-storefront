1120a665fec7a49cd2e2a62dafc27563ccac95b9
Biola <52200708+decorouz@users.noreply.github.com>
2022-05-18 20:58:11 +0100

    Add taggedmanager and migration file

--- tags/models.py
+++ tags/models.py
@@ -3,13 +3,23 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
+class TaggedItemManager(models.Manager):
+    def get_tag_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects.select_related("tag").filter(
+            target_ct=content_type, target_id=obj_id
+        )
+
+
 # Create your models here.
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
-class TagItem(models.Model):
+class TaggedItem(models.Model):
     # Type -> Product, Video, Project, Blog Post, Course
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     target_ct = models.ForeignKey(ContentType, on_delete=models.CASCADE)  # content type
     target_id = models.PositiveIntegerField()  # object id
