e7f3231c0b52e76ed967de9a325e921adae315ce
Biola <52200708+decorouz@users.noreply.github.com>
2022-05-19 10:04:15 +0100

    Implement prepopulated fields and autocomplete fields

--- store/admin.py
+++ store/admin.py
@@ -22,6 +22,8 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["title"]}
+    autocomplete_fields = ["collection"]
     actions = ["clear_inventory"]
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
@@ -78,6 +80,7 @@ class OrderAdmin(admin.ModelAdmin):
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ["title", "products_count"]
+    search_fields = ["title"]
 
     @admin.display(ordering="products_count")
     def products_count(self, collection):
