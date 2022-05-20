cfce634c68cfc67e155a00ba70be4694214aa63e
Biola <52200708+decorouz@users.noreply.github.com>
2022-05-20 15:53:54 +0100

    Implement DefaultRouter in url

--- store/urls.py
+++ store/urls.py
@@ -1,14 +1,25 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
-urlpatterns = [
-    path("products/", views.ProductList.as_view()),
-    path("products/<int:pk>/", views.ProductDetail.as_view()),
-    path("collections/", views.CollectionList.as_view()),
-    path(
-        "collections/<int:pk>/",
-        views.CollectionDetail.as_view(),
-        name="collection-detail",
-    ),
-]
+router = DefaultRouter()
+
+router.register(r"products", views.ProductViewSet)
+router.register(r"collections", views.CollectionViewSet)
+
+
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path("products/", views.ProductList.as_view()),
+#     path("products/<int:pk>/", views.ProductDetail.as_view()),
+#     path("collections/", views.CollectionList.as_view()),
+#     path(
+#         "collections/<int:pk>/",
+#         views.CollectionDetail.as_view(),
+#         name="collection-detail",
+#     ),
+# ]
