6d023d37c63ea27b09fd19f97b593f9d58954ec7
Biola <52200708+decorouz@users.noreply.github.com>
2022-05-21 10:47:58 +0100

    Implement routers in url

--- store/urls.py
+++ store/urls.py
@@ -5,21 +5,8 @@ from . import views
 
 router = DefaultRouter()
 
-router.register(r"products", views.ProductViewSet)
-router.register(r"collections", views.CollectionViewSet)
-
+router.register("products", views.ProductViewSet, basename="products")
+router.register("collections", views.CollectionViewSet)
 
 
 urlpatterns = router.urls
-
-
-# urlpatterns = [
-#     path("products/", views.ProductList.as_view()),
-#     path("products/<int:pk>/", views.ProductDetail.as_view()),
-#     path("collections/", views.CollectionList.as_view()),
-#     path(
-#         "collections/<int:pk>/",
-#         views.CollectionDetail.as_view(),
-#         name="collection-detail",
-#     ),
-# ]
