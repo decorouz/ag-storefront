fba6ee0ec173f58a312a0508ee3c59ff6e24a019
Biola <52200708+decorouz@users.noreply.github.com>
2022-05-29 11:53:18 +0100

    add ProductImageAdmin to core app

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from atexit import register
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
-from store.admin import ProductAdmin
+from store.admin import ProductAdmin, ProductImageInline
 from store.models import Product
 from tags.models import TaggedItem
 
@@ -39,7 +39,7 @@ class TagInline(GenericTabularInline):
 
 
 class CustomProductAdmin(ProductAdmin):
-    inlines = [TagInline]
+    inlines = [TagInline, ProductImageInline]
 
 
 admin.site.unregister(Product)
