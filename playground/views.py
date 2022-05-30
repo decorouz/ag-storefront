48e5afbb29ea0dfe7a64243b8ada5156efdc0290
Biola <52200708+decorouz@users.noreply.github.com>
2022-05-30 21:35:09 +0100

    Implement low level caching

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,14 @@
+import requests
+from django.core.cache import cache
 from django.shortcuts import render
-
-from .tasks import notify_customers
 
 
 # Create your views here.
 def say_hello(request):
-    notify_customers.delay("Hello")
-    return render(request, "hello.html")
+    key = "httpbib_result"
+    if cache.get(key) is None:
+        response = requests.get("https://httpbin.org/delay/2")
+        data = response.json()
+        cache.set(key, data)
+
+    return render(request, "hello.html", {"name": cache.get(key)})
